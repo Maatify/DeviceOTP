1371ba62a002332e7e7d9998c9db4207b3a11cac
Mohamed Abdulalim <megyptm@gmail.com>
2025-02-05 09:59:55 +0200

    3.0.0012

--- otp_manager/OTPRoleChecker.php
+++ otp_manager/OTPRoleChecker.php
@@ -1,12 +1,20 @@
 <?php
 /**
- * Created by Maatify.dev
- * User: Maatify.dev
- * Date: 2025-01-31
- * Time: 21:11
- * Project: DeviceOTP
- * IDE: PhpStorm
- * https://www.Maatify.dev
+ * @copyright   ©2025 Maatify.dev
+ * @Liberary    DeviceOTP
+ * @Project     DeviceOTP
+ * @author      Mohamed Abdulalim (megyptm) <mohamed@maatify.dev>
+ * @since       2025-01-31 21:11 PM
+ * @see         https://www.maatify.dev Maatify.com
+ * @link        https://github.com/Maatify/DeviceOTP  view project on GitHub
+ * @link        https://github.com/Maatify/AppHandler  (maatify/app-handler)
+ * @note        This Project using for OTP with MYSQL PDO (PDO_MYSQL).
+ * @note        This Project extends other libraries maatify/app-handler.
+ *
+ * @note        This program is distributed in the hope that it will be useful - WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
  */
 
 declare(strict_types=1);
