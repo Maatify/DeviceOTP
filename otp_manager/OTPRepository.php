1cd93745aa97a4e93d54e31bbba6978f13979ee8
Mohamed Abdulalim <megyptm@gmail.com>
2025-02-08 20:42:00 +0200

    5.0.0005
    Fix Bug within method confirmOTP() in OTPRepository Class

--- otp_manager/OTPRepository.php
+++ otp_manager/OTPRepository.php
@@ -262,13 +262,15 @@ class OTPRepository implements OTPRepositoryInterface
                             ':otp_id'            => $otpRow['otp_id'],
                         ]);
 
-                        $this->otpSenderTypeId = (new OTPSenderTypeIdService($this->otpSenderTypeId))->validate($otpRow['otp_sender_type_id']);
+                        $this->otpSenderTypeId = (new OTPSenderTypeIdService($this->otpSenderTypeId))->validate((int)$otpRow['otp_sender_type_id']);
+                        $this->otp_sender_type_id = (int)$otpRow['otp_sender_type_id'];
                     }
 
                     return 200;
                 } else {
                     // Expired Code
-                    $this->otpSenderTypeId = (new OTPSenderTypeIdService($this->otpSenderTypeId))->validate($otpRow['otp_sender_type_id']);
+                    $this->otpSenderTypeId = (new OTPSenderTypeIdService($this->otpSenderTypeId))->validate((int)$otpRow['otp_sender_type_id']);
+                    $this->otp_sender_type_id = (int)$otpRow['otp_sender_type_id'];
                     return 410;
                 }
             }
